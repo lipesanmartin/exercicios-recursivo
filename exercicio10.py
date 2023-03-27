7479fbf96fecef996657abb1baf018b643d4f277
LipeSanmartin <lipesanmartin@hotmail.com>
2023-03-27 01:46:37 -0300

    no message

---
+++ exercicio10.py
@@ -0,0 +1,17 @@
+#  10) Escreva uma função recursiva que determine quantas vezes um dígito K ocorre em um número natural N.
+#  Por exemplo, o dígito 2 ocorre 3 vezes em 762021192.
+
+
+def contar_caracteres(n, k):
+    if n == 0:
+        return 0
+    ultimo_digito = n % 10
+    if ultimo_digito == k:
+        return 1 + contar_caracteres(n // 10, k)
+    else:
+        return contar_caracteres(n // 10, k)
+
+num = int(input("Insira o numero: "))
+char = int(input("Insira qual numero quer contar a ocorrencia: "))
+
+print(contar_caracteres(num, char))
---
+++ exercicio8.py
@@ -0,0 +1,14 @@
+#  8) O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por x e y.
+#  Escreva uma função recursiva MDC em Python, que retorna o máximo divisor comum de x e y.
+#  O MDC de x e y é definido como segue: se y é igual a 0, então MDC(x,y) é x; caso contrário,
+#  MDC(x,y) é MDC (y, x%y), onde % é o operador resto.
+
+def mdc(x, y):
+    if y == 0:
+        return x
+    return mdc(y, x % y)
+
+numero1 = int(input("Insira o primeiro numero: "))
+numero2 = int(input("Insira o segundo numero: "))
+
+print(mdc(numero1, numero2))
