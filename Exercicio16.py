ce5cbdceb9caed81cab94044127082d0f6b45a47
LipeSanmartin <lipesanmartin@hotmail.com>
2023-03-27 01:13:23 -0300

    no message

---
+++ Exercicio16.py
@@ -0,0 +1,19 @@
+#  16) A função fatorial duplo é definida como o produto de todos os números naturais ímpares de 1 até algum
+#  número natural ímpar N. Assim, o fatorial duplo de 5 é 5!! = 1 * 3 * 5 = 15 Faça uma função recursiva que receba
+#  um número inteiro positivo ímpar N e retorne o fatorial duplo desse número.
+
+
+def fatorial_duplo(n: int) -> int:
+    if n <= 1:
+        return 1
+    return n * fatorial_duplo(n - 2)
+
+
+while True:
+    num = int(input("Insira um número impar positivo: "))
+    if num % 2 == 0 or num < 1:
+        print("Número inválido:")
+    else:
+        print(fatorial_duplo(num))
+        break
+
