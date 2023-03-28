700d855ba253e0a3071d1c4c0374a8bfe8387603
Felipe Sanmartin <lipesanmartin@hotmail.com>
2023-03-27 23:25:06 -0300

    no message

--- exercicio15.py
+++ exercicio15.py
@@ -1,10 +1,10 @@
 #  Faça uma função recursiva que receba um número inteiro positivo par N
 #  e imprima todos os números pares de 0 até N em ordem decrescente.
 
-def pares_crescentes(n: int) -> int:
+def pares_decrescentes(n: int) -> int:
     if n <= 0:
         return 0
-    return 1 + pares_crescentes(n - 1)
+    return 1 + pares_decrescentes(n - 1)
 
 # print(pares_crescentes(10))
 
@@ -16,4 +16,4 @@ while True:
         print("Número inválido, tente novamente.")
 
 for x in range(numero, -1, -2):
-    print(pares_crescentes(x))
+    print(pares_decrescentes(x))
