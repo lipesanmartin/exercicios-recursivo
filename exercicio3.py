d87aba64ca7e71400753cc13609881a802dfab15
Felipe Sanmartin <lipesanmartin@hotmail.com>
2023-04-03 16:15:04 -0300

    no message

--- exercicio3.py
+++ exercicio3.py
@@ -1,2 +1,11 @@
 #  3) Faça uma função recursiva que permita inverter um número inteiro N. Ex: 123 – 321
 
+def inverter_inteiro(numero):
+    if numero < 10:
+        return numero
+    ultimo_digito = numero % 10
+    resto_do_numero = numero // 10
+    numero_invertido = inverter_inteiro(resto_do_numero)
+    return int(str(ultimo_digito) + str(numero_invertido))
+
+print(inverter_inteiro(123456789))
--- exercicio7.py
+++ exercicio7.py
@@ -1,2 +1,13 @@
 # 7) Crie um programa em Python que receba um vetor de números reais com 100 elementos.
 #  Escreva uma função recursiva que inverta ordem dos elementos presentes no vetor.
+
+def inverter_vetor(vetor, inicio, fim):
+    if inicio < fim:
+        vetor[inicio], vetor[fim] = vetor[fim], vetor[inicio]
+        inverter_vetor(vetor, inicio + 1, fim - 1)
+
+
+numeros = [i for i in range(1, 101)]
+print("Vetor original:", numeros)
+inverter_vetor(numeros, 0, 99)
+print("Vetor invertido:", numeros)
